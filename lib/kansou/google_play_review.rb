18e5cb8ce775bcb0e15099de652732d7bc5b2c6a
punchdrunker <nanao.hhk@gmail.com>
2015-12-22 14:33:46 +0900

    stop using csv

--- lib/kansou/google_play_review.rb
+++ lib/kansou/google_play_review.rb
@@ -1,8 +1,10 @@
+require 'json'
+require 'oga'
+
 module Kansou
   class GooglePlayReview
-    def initialize(app_id, bucket_id, csv_dir=nil, gsutil_path=nil)
+    def initialize(app_id)
       return nil unless app_id
-      return nil unless bucket_id
 
       @app_id = app_id
     end
@@ -25,14 +27,58 @@ module Kansou
         path = "/store/getreviews"
         data = "id=#{app_id}&reviewSortOrder=0&reviewType=1&pageNum=#{page}"
         response = http.post(path, data)
-        html_string = JSON.parse(response.body.gsub(/\)\]\}\'/,""))[0][2]
       end
 
-      def parse(html_text)
-        p html_text
+      def parse(input_text)
+        input_text = remove_unused_lines(input_text)
+        json = JSON.load(input_text)
+        body = json[0][2]
+
+        titles = []
+        bodies = []
+        dates = []
+        stars = []
+        authors = []
+        document = Oga.parse_html(body)
+        main_expression = 'div[class="single-review"]'
+        title_expression = 'span[class="review-title"]'
+        body_expression = 'div[class="review-body"]'
+        date_expression = 'span[class="review-date"]'
+        star_expression = 'div[class="current-rating"]'
+        author_expression = 'span[class="author-name"]'
+
+        document.css(main_expression).each do |elm|
+          elm.css(title_expression).each { |title_node| titles.push(title_node.text) }
+          elm.css(body_expression).each { |body_node| bodies.push(body_node.text) }
+          elm.css(date_expression).each { |date_node| dates.push(date_node.text) }
+          elm.css(star_expression).each { |star_node| stars.push(process_star_count(star_node)) }
+          elm.css(author_expression).each { |author_node| authors.push(author_node.text.strip) }
+        end
+
         reviews = []
+        count = stars.size - 1
+        (0..count).each do |key|
+          item = {
+            :star   => stars[key],
+            :user   => authors[key],
+            :date   => dates[key],
+            :title  => titles[key],
+            :body   => bodies[key],
+            :app_id => @app_id
+          }
+          reviews.push(item)
+        end
         return reviews
       end
 
+      def remove_unused_lines(text)
+        lines =  text.split("\n")
+        return lines[2] + "]"
+      end
+
+      def process_star_count(node)
+        widths = node.attribute("style").value.match("width: ([0-9]+)")
+        star  = widths[1].to_i / 20
+      end
   end
 end
--- spec/kansou/google_play_review_spec.rb
+++ spec/kansou/google_play_review_spec.rb
@@ -2,19 +2,18 @@ require 'spec_helper'
 
 describe Kansou::GooglePlayReview do
   it 'parses google play reviews' do
-    dummy_csv_path = File.dirname(__FILE__) + '/../google_play_reviews.csv'
-    utf16 = open(dummy_csv_path, "rb:BOM|UTF-16"){|f| f.read }
-    utf8 = utf16.encode("UTF-8")
+    dummy_review_path = File.dirname(__FILE__) + '/../google-review.txt'
+    dummy = open(dummy_review_path){|f| f.read }
 
-    review = Kansou::GooglePlayReview.new(23134, "pubsite_prod_rev_1234567890")
-    review.stub(:download).and_return(utf8)
+    review = Kansou::GooglePlayReview.new("jp.mixi")
+    review.stub(:download).and_return(dummy)
 
     reviews = review.fetch(1)
     
-    expect(reviews.size).to eq(2)
-    expect(reviews[1][:star]).to eq("5")
-    expect(reviews[1][:date].to_s).to eq("2015-11-02 15:40:54 +0900")
-    expect(reviews[1][:title].length).not_to eq(0)
-    expect(reviews[1][:body].length).not_to eq(0)
+    expect(reviews.size).to eq(40)
+    expect(reviews[2][:star]).to eq(1)
+    expect(reviews[2][:date].to_s).to eq("2015年12月21日")
+    expect(reviews[2][:title].length).not_to eq(0)
+    expect(reviews[2][:body].length).not_to eq(0)
   end
 end
