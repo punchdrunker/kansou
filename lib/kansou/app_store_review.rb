bb990fc22eff16ab82d8f82580e3b170af8a3111
SideCI <support@sideci.com>
2016-03-14 09:32:43 +0000

    Applied fixes from SideCI

--- lib/kansou/app_store_review.rb
+++ lib/kansou/app_store_review.rb
@@ -16,25 +16,25 @@ module Kansou
       @bodies = []
     end
 
-    def fetch(page_amount=1)
+    def fetch(page_amount = 1)
       reviews = []
       max_page = page_amount - 1
       (0..max_page).each do |page|
         xml = download(page)
         reviews.concat(parse(xml))
       end
-      return reviews
+      reviews
     end
 
-    def download(page=0)
-      base_url = "https://itunes.apple.com"
-      user_agent = "iTunes/9.2 (Windows; Microsoft Windows 7 "\
-                                + "Home Premium Edition (Build 7600)) AppleWebKit/533.16"
+    def download(page = 0)
+      base_url = 'https://itunes.apple.com'
+      user_agent = 'iTunes/9.2 (Windows; Microsoft Windows 7 '\
+                                + 'Home Premium Edition (Build 7600)) AppleWebKit/533.16'
 
-      url = base_url + "/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id="\
-        + @app_id.to_s + "&pageNumber="+page.to_s+"&sortOrdering=4&type=Purple+Software"
+      url = base_url + '/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id='\
+        + @app_id.to_s + '&pageNumber=' + page.to_s + '&sortOrdering=4&type=Purple+Software'
       xml = open(url, 'User-Agent' => user_agent, 'X-Apple-Store-Front' => '143462-1').read
-      return xml
+      xml
     end
 
     def parse(xml)
@@ -50,66 +50,59 @@ module Kansou
       end
 
       document.css('HBoxView[topInset="1"]').each do |elm|
-        @stars.push elm.attribute("alt").value.gsub(/ stars*/,"")
+        @stars.push elm.attribute('alt').value.gsub(/ stars*/, '')
       end
 
       document.css('TextView[styleSet="normal11"]').each do |elm|
-        @bodies.push(elm.text.gsub("\n", "<br />"))
+        @bodies.push(elm.text.gsub("\n", '<br />'))
       end
 
       items = []
       count = @stars.size - 1
       (0..count).each do |key|
         item = {
-          :star => @stars[key],
-          :user => @users[key],
-          :date => @dates[key],
-          :title => @titles[key],
-          :body => @bodies[key],
-          :version => @versions[key],
-          :app_id => @app_id
+          star: @stars[key],
+          user: @users[key],
+          date: @dates[key],
+          title: @titles[key],
+          body: @bodies[key],
+          version: @versions[key],
+          app_id: @app_id
         }
         items.push(item)
       end
-      return items
+      items
     end
 
     def parse_author_info(document)
       expression = 'TextView[topInset="0"][styleSet="basic13"][squishiness="1"][leftInset="0"][truncation="right"][textJust="left"][maxLines="1"]'
       document.css(expression).each do |elm|
-        if elm.text =~ /by/
-          tmp_array = elm.text.gsub(" ", "").split("\n")
-          info = []
-          tmp_array.each do |v|
-            if v!=""&&v!="-"&&v!="by"
-              info.push v
-            end
-          end
-          @users.push info[0]
+        next unless elm.text =~ /by/
+        tmp_array = elm.text.delete(' ').split("\n")
+        info = []
+        tmp_array.each do |v|
+          info.push v if v != '' && v != '-' && v != 'by'
+        end
+        @users.push info[0]
 
-          if info[1]
-            @versions.push(get_version(info[1]))
-          else
-            @versions.push("")
-          end
+        if info[1]
+          @versions.push(get_version(info[1]))
+        else
+          @versions.push('')
+        end
 
-          if info[2]
-            @dates.push(info[2])
-          else
-            @dates.push("")
-          end
+        if info[2]
+          @dates.push(info[2])
+        else
+          @dates.push('')
         end
       end
     end
 
-
     def get_version(text)
       version = nil
-      if /Version([\d\.]+)/ =~ text
-        version = $1
-      end
-      return version
+      version = Regexp.last_match(1) if /Version([\d\.]+)/ =~ text
+      version
     end
-
   end
 end
